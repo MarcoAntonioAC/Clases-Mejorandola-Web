1567577a966f62a9e98d989a0488be39051db753
MarcoAntonioAC <marco_antonio1997@hotmail.com>
2013-08-07 20:17:22 -0500

    Cuarta Clase
    Introducción a Javascript y mejora en la organización. Se esperan
    correcciones en las rutas de algunos archivos.

---
+++ res/js/ejemplo3.js
@@ -0,0 +1,12 @@
+function max(a,b){
+	if(a>b){
+		console.log('este si va a salir')
+		return a
+		console.log('esto no va a salir')
+	}
+	return b
+}
+max(6,3)
+max(1,9)
+max(200,200)
+
---
+++ res/js/ejemplo4.js
@@ -0,0 +1,7 @@
+var a = [1,2,3,2],
+	sum = 0
+
+for(var i=0; i<a.length;i++){
+	sum += a[i]
+	console.log(i, sum)
+}
